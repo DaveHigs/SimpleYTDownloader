be11c1b81e510f1ca4f2da630ecaf49cdc66ee60
David <david.higuera1@hotmail.com>
2023-10-24 01:33:47 -0300

    Initial GUI framework and function implementation

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-import customtkinter
+import os
+import sys
+import customtkinter as tk
+from pytube import YouTube
 
-customtkinter.set_appearance_mode('system')
-#customtkinter.set_default_color_theme('dark-blue')
+# Function to download as mp4
+def download_video():
+    video_url = url_entry.get()
+    if formatmenu.get() == 'MP4':
+        try:
+            yt = YouTube(video_url)
+            stream = yt.streams.get_highest_resolution()
+            # download_path = os.path.expanduser("~")  # Change this to your desired download path
+            download_path = os.getcwd() # Uses CWD
+            stream.download(output_path=download_path)
+            status_label.configure(text="Download successful!")
+        except Exception as e:
+            status_label.configure(text="Error: " + str(e))
+    else:
+        pass
 
-root = customtkinter.CTk()
+# Create the main window
+root = tk.CTk()
 root.geometry('500x500')
-root.title('Youtube Downloader')
+root.title("YouTube Downloader")
+root.minsize(350, 350)
 
-def test():
-    print('Test')
+# Create the main frame
+frame = tk.CTkFrame(master=root)
+frame.pack(pady=20, padx=20, fill='both', expand=True, side='bottom')
 
-def choose_resolution(choice):
-    print(choice)
+# Youtube URL
+url_label = tk.CTkLabel(frame, text="Enter YouTube URL:")
+url_label.pack()
 
-def access_resolution():
-    button.configure(state='disabled')
+url_entry = tk.CTkEntry(frame, width=400)
+url_entry.pack(padx=10)
 
-    labelres = customtkinter.CTkLabel(master=frame, text='Choose Resolution', font=('Roboto', 12))
-    labelres.pack(pady=3, padx=10)
+# Format Selector
+format_label = tk.CTkLabel(frame, text="Choose Download Format:")
+format_label.pack(pady=5)
 
-    optionmenu = customtkinter.CTkOptionMenu(master=frame, values=['360p', '720p', '1080p'], command=choose_resolution)
-    optionmenu.pack(pady=12, padx=10)
+formatmenu = tk.CTkOptionMenu(frame, values=['MP4', 'MP3'], anchor='center')
+formatmenu.pack(padx=10)
 
+# Download Button
+download_button = tk.CTkButton(frame, text="Download", command=download_video)
+download_button.pack(pady=10, padx=10)
 
+# Create a status label
+status_label = tk.CTkLabel(frame, text="")
+status_label.pack()
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill='both', expand=True)
-
-label = customtkinter.CTkLabel(master=frame, text='Simple Youtube Downloader', font=('Roboto', 24))
-label.pack(pady=12, padx=10)
-
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text='Insert Link', width=350)
-entry1.pack(pady=12, padx=10)
-
-button = customtkinter.CTkButton(master=frame, text='Start Download', command=access_resolution)
-button.pack(pady=12, padx=10)
-
+# Start the GUI main loop
 root.mainloop()
