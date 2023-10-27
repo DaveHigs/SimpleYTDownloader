f6e54413e4c24c893fa9833e3356aab9a80fd47e
David <david.higuera1@hotmail.com>
2023-10-26 21:46:57 -0300

    Adds docstring for webview_folder_dialog function

--- folder_chooser.py
+++ folder_chooser.py
@@ -2,6 +2,19 @@ import os
 import webview
 
 def webview_folder_dialog():
+    '''
+    Creates a dialog window that allows the user to pick a folder path.
+
+    Args:
+        None
+    
+    Returns:
+        file (str) or None: A string with the choosen folder path, or None if 
+        the user closes the window.
+
+    Raises:
+        TypeError: If user exits the folder dialog without picking a location.
+    '''
     file = None
     def choose_folder_dialog(w):
         nonlocal file
@@ -9,12 +22,10 @@ def webview_folder_dialog():
             default_dir = os.path.expanduser("~/Downloads")
             file = w.create_file_dialog(webview.FOLDER_DIALOG, directory=default_dir)[0]
         except TypeError:
-            pass  # user exited file dialog without picking
+            pass
         finally:
             w.destroy()
     window = webview.create_window('', hidden=True)
     webview.start(choose_folder_dialog, window)
-    # file will either be a string or None
-    return file
 
-print(webview_folder_dialog())
+    return file
